607e391fe2ea9260c1bd181640fe8baf71c4cb12
The.Drake1010 <akhilsingh707@gmail.com>
2019-02-23 23:37:10 +0530

    Added Utility functions and voter account
    The voter account was added with basic initialization procedure. Apart from this, utility functions for signing and hashing were added

---
+++ Utility.py
@@ -0,0 +1,40 @@
+# Utility.py
+# Houses all the security algorithms and procedures
+# required to generate and verify signatures and accounts
+
+import ecdsa
+import hashlib
+
+
+
+def applySHA256(message):												#hashing messages using SHA256 algorithm
+	hashMsg = hashlib.sha256((str(message).encode())).hexdigest()
+	return hashMsg
+
+
+def generateKeyPair():
+	sk = ecdsa.SigningKey.generate(curve=ecdsa.SECP256k1)				#Use Elliptic Curve to generate secret key "sk"
+	pk = sk.get_verifying_key()											#Use sk to generate public key "pk"
+	return [sk,pk]														#Return both the keys
+
+
+def applySignature(sk,inputdata):										#Generates Signature of inputdata with help of secret key
+	sig = sk.sign((str(inputdata)).encode())
+	return sig
+
+
+def verifySignature(pk,data,sig):
+	return pk.verify(sig,(str(data)).encode())
+
+
+
+
+
+
+## BEYOND THIS LINE, EVERYTHING IS FOR TESTING PURPOSES ONLY...
+
+if __name__ == "__main__":
+	[sk,pk] = generateKeyPair()
+	data = input("Enter message to hash: ")
+	print("The hashed msg is: "+ applySHA256(data))
+	
