cf40a864ed6ca86fe5140ecb17bd6c87d1183dc5
The.Drake1010 <akhilsingh707@gmail.com>
2019-02-25 00:13:45 +0530

    Adding description to unknown files

--- DBQuery.py
+++ DBQuery.py
@@ -1,12 +1,17 @@
+# DBQuery.py
+# Houses connections to database to query a voter's details from database
+
+
+
 import hashlib
 import uuid
 import datetime as dt
 import sqlite3
 from sqlite3 import Error
 import os
-import Accounts
 import Utility as util
-import ecdsa
+
+
 
 def getpublicKey(name,dob):
 	try:
@@ -82,14 +87,13 @@ def deleteData(name,dob,v_id):
 
 
 
+##TESTING...
+
 if __name__ == '__main__':
+
 	privateKey = b"\xad\xd1\xdb\xe86\x07\xc4\x1b\x18\x9b\xaa\x98\x85v|U\xf8\xfa\xad \x11\xeb\x8a\x1f\x1d/\x13\xf5\xe86\xb1\xed"
-	#sk = ecdsa.SigningKey.from_string(privateKey,curve = ecdsa.SECP256k1) 
 	publicKey = getpublicKey("Polly Robertson","2018-11-12")
-	#pk = ecdsa.VerifyingKey.from_string(publicKey,curve = ecdsa.SECP256k1) 
 	data = "Hello World"
 	sign = util.applySignature(privateKey,data)
-	print(util.verifySignature(publicKey,data,sign))
-	#sign=sk.sign((str(inputdata)).encode())
-	#print(pk.verify(sign,(str(inputdata)).encode()))
+	print(util.verifySignature(publicKey,"Hello World",sign))
 
